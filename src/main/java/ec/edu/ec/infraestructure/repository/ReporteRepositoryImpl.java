15dcdb1f007c6d87dd65163512c75edd3e9d1f0a
Andy Suquilandi <aksuquilandi@uce.edu.ec>
2026-07-15 18:49:51 -0500

    Taller33:programacionReactiva

--- src/main/java/ec/edu/ec/infraestructure/repository/ReporteRepositoryImpl.java
+++ src/main/java/ec/edu/ec/infraestructure/repository/ReporteRepositoryImpl.java
@@ -4,6 +4,7 @@ import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
 
+
 @Transactional
 @ApplicationScoped
 public class ReporteRepositoryImpl implements PanacheRepositoryBase<Reporte, Integer>{
--- src/main/java/ec/edu/ec/web/resource/ReporteResource.java
+++ src/main/java/ec/edu/ec/web/resource/ReporteResource.java
@@ -3,14 +3,27 @@ package ec.edu.ec.web.resource;
 
 
 
+import java.util.List;
+
 import ec.edu.ec.application.service.ReporteService;
 import ec.edu.ec.domain.model.Reporte;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
 
+@Path("/reportes")
 public class ReporteResource {
     @Inject
     private ReporteService reporteService;
+    //reporte/todos
+    
     public Reporte buscarporId(Integer Id){
         return this.reporteService.buscaReporteId(Id);
+    }
+
+    
+    @Path("/todos")
+    @GET    public List<Reporte> buscarTodos(){
+        return this.reporteService.buscarTodos();
     }
 }
